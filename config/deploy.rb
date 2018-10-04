7936d3af8e74c9942b72576b41a358d130d4845d
bondar5471 <bondar54711745@gmail.com>
2018-10-04 16:19:43 +0300

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, "QnA"
+set :repo_url, "git@github.com:bondar5471/qna.git"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deploy/qna"
+set :deploy_user, "deploy"
+
+# Default value for :linked_files is []
+append :linked_files, %w{config/database.yml}
+# Default value for linked_dirs is []
+append :linked_dirs,"bin", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads","vendot/bundle"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
---
+++ config/deploy/production.rb
@@ -0,0 +1,13 @@
+server "18.130.118.104", user: "deploy", roles: %w{app db web}, primary: true
+
+role :app, %w{deploy@18.130.118.104}
+role :web, %w{deploy@18.130.118.104}
+role :db, %w{deploy@18.130.118.104}
+
+set :rails_env, :production
+
+set :ssh_options, {
+  keys: %w(/home/bondar/proj_ruby/QnAEs.pem),
+  forward_agent: true,
+  auth_methods: %w(publickey password)
+}
