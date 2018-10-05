d53838cdf7bc1e247939935840589a88f1bd293b
bondar5471 <bondar54711745@gmail.com>
2018-10-05 13:01:16 +0300

    deploy error

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,15 +14,3 @@ set :deploy_user, 'deploy'
 append :linked_files, 'config/database.yml', 'config/private_pub.yml', '.env'
 # Default value for linked_dirs is []
 append :linked_dirs, 'bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads', 'vendot/bundle'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
