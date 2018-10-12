d103ead66b7a599b0b8484e386c0d2e52106b791
bondar5471 <bondar54711745@gmail.com>
2018-10-12 10:08:32 +0300

    Fix index

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,38 +26,38 @@ namespace :deploy do
   after :publishing, :restart
 end
 
-namespace :private_pub do
-  desc 'Start private_pub server'
-  task :start do
-    on roles(:app) do
-      within current_path do
-        with rails_env: fetch(:rails_env) do
-          execute :bundle, 'exec thin -C /qna/shared/config/private_pub_thin.yml start'
-        end
-      end
-    end
-  end
+# namespace :private_pub do
+#   desc 'Start private_pub server'
+#   task :start do
+#     on roles(:app) do
+#       within current_path do
+#         with rails_env: fetch(:rails_env) do
+#           execute :bundle, 'exec thin -C /qna/shared/config/private_pub_thin.yml start'
+#         end
+#       end
+#     end
+#   end
 
-  desc 'Stop private_pub server'
-  task :stop do
-    on roles(:app) do
-      within current_path do
-        with rails_env: fetch(:rails_env) do
-          execute :bundle, 'exec thin -C /qna/shared/config/private_pub_thin.yml stop'
-        end
-      end
-    end
-  end
+#   desc 'Stop private_pub server'
+#   task :stop do
+#     on roles(:app) do
+#       within current_path do
+#         with rails_env: fetch(:rails_env) do
+#           execute :bundle, 'exec thin -C /qna/shared/config/private_pub_thin.yml stop'
+#         end
+#       end
+#     end
+#   end
 
-  desc 'Restart private_pub server'
-  task :restart do
-    on roles(:app) do
-      within current_path do
-        with rails_env: fetch(:rails_env) do
-          execute :bundle, 'exec thin -C /qna/shared/config/private_pub_thin.yml restart'
-        end
-      end
-    end
-  end
-end
-after 'deploy:restart', 'private_pub:restart'
+#   desc 'Restart private_pub server'
+#   task :restart do
+#     on roles(:app) do
+#       within current_path do
+#         with rails_env: fetch(:rails_env) do
+#           execute :bundle, 'exec thin -C /qna/shared/config/private_pub_thin.yml restart'
+#         end
+#       end
+#     end
+#   end
+# end
+# after 'deploy:restart', 'private_pub:restart'
